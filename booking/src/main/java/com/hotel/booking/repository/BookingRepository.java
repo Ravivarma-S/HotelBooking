15ce787b2f2b6b464a150371dbc4a72692e81f5e
Guna <velakapuramguna@gmail.com>
2026-04-07 10:32:59 +0530

    removed booking

--- booking/src/main/java/com/hotel/booking/repository/BookingRepository.java
+++
@@ -1,6 +0,0 @@
-package com.hotel.booking.repository;
-
-import com.hotel.booking.entity.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookingRepository extends JpaRepository<Booking, Long> {}
--- booking/src/main/java/com/hotel/booking/repository/RoomRepository.java
+++
@@ -1,6 +0,0 @@
-package com.hotel.booking.repository;
-
-import com.hotel.booking.entity.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomRepository extends JpaRepository<Room, Long> {}
